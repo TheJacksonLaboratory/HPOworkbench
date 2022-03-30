d826ea94c5cbe612ed2709039fa203efa4fe8fa1
Peter Robinson <peter.robinson@jax.org>
2022-03-30 13:47:05 -0400

    output with HP labels

--- hpoworkbench-cli/src/main/java/org/monarchinitiative/hpoworkbench/cmd/PhenopacketCompareCommand.java
+++ hpoworkbench-cli/src/main/java/org/monarchinitiative/hpoworkbench/cmd/PhenopacketCompareCommand.java
@@ -5,6 +5,8 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.monarchinitiative.phenol.io.OntologyLoader;
+import org.monarchinitiative.phenol.ontology.data.Ontology;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 import org.phenopackets.schema.v2.Phenopacket;
 import org.phenopackets.schema.v2.core.Disease;
@@ -13,10 +15,7 @@ import picocli.CommandLine;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 import static org.apache.commons.math3.stat.inference.TestUtils.chiSquare;
@@ -46,6 +45,7 @@ import static org.apache.commons.math3.stat.inference.TestUtils.chiSquareTest;
  * --groupA
  * MONDO:0013847
  * --groupB MONDO:0013267
+ * --hpo <path>/hp.json
  */
 @CommandLine.Command(name = "phenopacketCompare",
         mixinStandardHelpOptions = true,
@@ -65,22 +65,57 @@ public class PhenopacketCompareCommand implements Callable<Integer> {
     @CommandLine.Option(names = {"-b", "--groupB"},required = true, description = "name of groupA")
     private String groupB;
 
+    @CommandLine.Option(names={"--hpo"}, description = "path to hp.json")
+    private String hpoJsonPath = null;
+
     @Override
     public Integer call() throws Exception {
         List<File> files = getAllPhenopacketFiles(new File(this.phenopacketDirectory));
         List<Phenopacket> packets = getPhenopackets(files);
         List<Phenopacket> packetsA = getGroupA(packets);
         List<Phenopacket> packetsB = getGroupB(packets);
+        Ontology hpo = null;
+        if (hpoJsonPath != null) {
+            hpo = OntologyLoader.loadOntology(new File(this.hpoJsonPath));
+        }
         System.out.printf("Group A n=%d, Group B n=%d\n", packetsA.size(), packetsB.size());
         Set<TermId> allHpoTermIds = getAllHpos(packets);
         for (TermId hpoId : allHpoTermIds) {
             long[][] counts = getCounts(hpoId, packetsA, packetsB);
             double chiSquare = chiSquare(counts);
             double chiSquareP = chiSquareTest(counts);
-            System.out.printf("%s: chi2 %.2f p-value %.2f\n", hpoId.getValue(), chiSquare, chiSquareP);
+            String hpoOut = hpoId.getValue();
+            if (hpo != null) {
+                Optional<String> labelOpt = hpo.getTermLabel(hpoId);
+                if (labelOpt.isPresent()) {
+                    String label = labelOpt.get();
+                    hpoOut = String.format("%s (%s)", label, hpoOut);
+                }
+            }
+            System.out.printf("%s: %s --  chi2 %.2f p-value %e\n",
+                    hpoOut, outputAandB(counts),
+                    chiSquare, chiSquareP);
         }
         return null;
     }
+
+
+
+    public String outputAandB(long[][] counts) {
+       long groupAannotated =  counts[0][0]; // group A with HPO
+        long groupAnotAnnotated = counts[0][1] ; // group A with HPO
+        long groupBannotated =counts[1][0]; // group A with HPO
+        long groupBnotAnnotated = counts[1][1]; // group A with HPO
+        long totalA = groupAannotated + groupAnotAnnotated;
+        double aPerc =100.0* (double) groupAannotated/totalA;
+        long totalB = groupBannotated + groupBnotAnnotated;
+        double bPerc = 100.0* (double) groupBannotated/totalB;
+        return String.format("%s: %d/%d (%.1f%%); %s: %d/%d (%.1f%%)",
+                this.groupA, groupAannotated, totalA, aPerc, this.groupB, groupBannotated, totalB, bPerc);
+
+    }
+
+
 
     private long[][] getCounts(TermId hpoId, List<Phenopacket> packetsA, List<Phenopacket> packetsB) {
         long[][]  counts = new long[2][2];
